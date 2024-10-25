2bdd787848cd2d8773f8162a67aa6953acdd97c3
ayabarakat3 <ayabarakat14@gmail.com>
2024-10-24 21:01:36 -0400

    Add event listener for product selection and quantity input

--- main.js
+++ main.js
@@ -6,4 +6,13 @@ const placeOrderButton = document.getElementById('place-order');
 const orderSummary = document.getElementById('order-summary');
 // By creating this code the code is now set up to be able to assign products and find them through their ID's.
 
-// Task 2: 
+// Task 2: Calculate total price when product or quantity changes.
+function updateTotalPrice() {
+const productPrice = parseFloat(productSelector.value);
+const quantity = parseInt(quantityInput.value);
+const totalPrice = productPrice * quantity;
+// This code is meant to calculate product prices when quantities differ. 
+// Code to find the total amount per quantity is now complete.
+totalPriceElement.textContent = totalPrice.toFixed(3);}
+// This code is set to display the total price to 3 decimal places.
+// Code to find the price and display the amount is now complete. 
