36b961205fe44a844c3490c3b01a56d16a81c879
ayabarakat3 <ayabarakat14@gmail.com>
2024-10-24 20:50:34 -0400

    Setup basic HTML structure for order form

---
+++ main.js
@@ -0,0 +1,9 @@
+// Task 1: Setting up base of the code to get the elements.
+const productSelector = document.getElementById('product-selector');
+const quantityInput = document.getElementById('quantity');
+const totalPriceElement = document.getElementById('total-price');
+const placeOrderButton = document.getElementById('place-order');
+const orderSummary = document.getElementById('order-summary');
+// By creating this code the code is now set up to be able to assign products and find them through their ID's.
+
+// Task 2: 
